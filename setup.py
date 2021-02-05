4bc1cd28d1024b32b54f4f70dac29310f9a5a662
Dustin Lo <dustinlo.pcc@gmail.com>
2021-02-05 12:31:43 -0800

    used ast library to parse DICT and LIST types (#8)
    * used ast library to parse DICT and LIST types
    
    removed usage of json library in param generation, only in file generation
    fixed unit tests
    fixed enum params in hysds-io
    calling List and Dict types "objects" in hysds-io
    
    * changed papermill version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='notebook_pge_wrapper',
-    version='1.0.0',
+    version='1.0.1',
     long_description='Library to generate hysds_io and job_specs for Jupyter notebooks',
     packages=find_packages(),
     include_package_data=True,
@@ -14,6 +14,6 @@ setup(
     },
     install_requires=[
         'click>=7.1.2',
-        'papermill==2.2.0',
+        'papermill>=2.2.0',
     ]
 )
