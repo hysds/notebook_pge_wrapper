caeda9b0d662cdf4be25a6ebff9c509dfe2357cf
jimmie <jimmie.d.young@gmail.com>
2021-11-01 16:48:43 -0700

    Add templates to be installed as package_data

--- setup.py
+++ setup.py
@@ -1,21 +1,33 @@
 from setuptools import setup, find_packages
 
-setup(
-    name='notebook_pge_wrapper',
-    version='1.0.3',
-    long_description='Library to generate hysds_io and job_specs for Jupyter notebooks',
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    entry_points={
-        'console_scripts': [
-            'notebook-pge-wrapper = notebook_pge_wrapper.cli:cli'
-        ]
-    },
-    install_requires=[
-        'click>=7.1.2',
-        'Jinja2>=2.11.3',
-        'PyYAML>=5.4.1',
-        'papermill>=2.2.0',
-    ]
-)
+def do_setup():
+    setup(
+        name='notebook_pge_wrapper',
+        version='1.0.3',
+        long_description='Library to generate hysds_io and job_specs for Jupyter notebooks',
+        packages=find_packages(),
+        include_package_data=True,
+        zip_safe=False,
+        entry_points={
+            'console_scripts': [
+                'notebook-pge-wrapper = notebook_pge_wrapper.cli:cli'
+            ]
+        },
+        install_requires=[
+            'click>=7.1.2',
+            'Jinja2>=2.11.3',
+            'PyYAML>=5.4.1',
+            'papermill>=2.2.0',
+        ],
+        package_data={
+            'notebook_pge_wrapper' : [
+                'templates/Dockerfile.template',
+                'templates/*.ipynb',
+                'templates/settings.yml',
+                'templates/README.md',
+             ],
+        },
+    )
+
+if __name__ == "__main__":
+    do_setup()
